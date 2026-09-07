40a39cedbc59f4c055ae5b5f1e14296434b3d1b4
Syed Umair Ali <syedumairal.617@gmail.com>
2026-09-07 15:20:37 +0500

    working on about section

---
+++ src/sections/about-section.jsx
@@ -0,0 +1,9 @@
+const AboutSection = () => {
+  return (
+    <section id="about" className="px-auto mt-44">
+      <div className="flex flex-col md:flex-row gap-14 md:gap-18 max-w-7xl mx-auto"></div>
+    </section>
+  );
+};
+
+export default AboutSection;
