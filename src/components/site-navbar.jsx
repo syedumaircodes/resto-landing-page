d8438fa7f882d5c37b4d8f7286db90be8318ff74
Syed Umair Ali <syedumairal.617@gmail.com>
2026-09-07 17:15:51 +0500

    added blur on scroll to navbar

--- src/components/site-navbar.jsx
+++ src/components/site-navbar.jsx
@@ -1,11 +1,21 @@
 import { MenuIcon, XIcon } from "lucide-react";
 import { navLinks } from "../data/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Sitenavbar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <nav className="fixed top-0 z-20 px-auto w-full transition-all duration-300 bg-transparent">
+      <nav
+        className={`fixed top-0 z-20 px-auto w-full transition-all duration-300 ${scrolled ? "bg-white/70 backdrop-blur-md" : "bg-transparent"} `}
+      >
         <div className="flex items-center justify-between font-medium py-4 mx-auto max-w-7xl">
           <a href="/">
             <img src="/assets/logo.svg" alt="logo" />
